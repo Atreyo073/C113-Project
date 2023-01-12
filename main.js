70b0a373635e3eb71130cd0ad819881f32cbb6bf
Atreyo073 <107910400+Atreyo073@users.noreply.github.com>
2023-01-12 19:49:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+function draw()
+{
+    circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+ellipse(56, 46, 55, 55);
+describe('white ellipse with black outline in middle of a gray canvas');
+rect(30, 20, 55, 55, 20);
+describe(
+  'white rect with black outline and round edges in mid-right of canvas'
+);
+}
